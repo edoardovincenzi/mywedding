f35b5b81f409e825ca168bb0b8cb48a74135b07d
edoardovincenzi <edoardo.vincenzi95@gmail.com>
2023-02-01 14:19:48 +0100

    import icon

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import 'leaflet/dist/leaflet.css'; // <- Leaflet styles
+import 'leaflet/dist/images/marker-shadow.png';
+import 'leaflet/dist/images/marker-icon.png';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
