62d30f59aa71b54da9cabbd1567f44d9e0429850
edoardovincenzi <edoardo.vincenzi95@gmail.com>
2023-02-01 14:28:09 +0100

    add icon

--- src/components/MapTimeBlock.tsx
+++ src/components/MapTimeBlock.tsx
@@ -1,4 +1,6 @@
 import { MapContainer, TileLayer, Marker, Tooltip } from 'react-leaflet';
+import markerIconPng from 'leaflet/dist/images/marker-icon.png';
+import { Icon } from 'leaflet';
 
 const MapTimeBlock = () => {
   return (
@@ -12,14 +14,32 @@ const MapTimeBlock = () => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[45.223865011261026, 10.784063829222905]}>
+      <Marker
+        position={[45.223865011261026, 10.784063829222905]}
+        icon={
+          new Icon({
+            iconUrl: markerIconPng,
+            iconSize: [25, 41],
+            iconAnchor: [20, 13],
+          })
+        }
+      >
         <Tooltip permanent direction="right">
           <span className="text-sm">
             <b>Villa corte Peron</b>
           </span>
         </Tooltip>
       </Marker>
-      <Marker position={[45.353534419661756, 10.730409461364351]}>
+      <Marker
+        position={[45.353534419661756, 10.730409461364351]}
+        icon={
+          new Icon({
+            iconUrl: markerIconPng,
+            iconSize: [25, 41],
+            iconAnchor: [20, 13],
+          })
+        }
+      >
         <Tooltip permanent direction="right">
           <span className="text-sm">
             <b>Castello di Valeggio sul Mincio</b>
