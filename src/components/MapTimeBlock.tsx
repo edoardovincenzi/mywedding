bef4808a11170a5d98938d58d094caaa6a72f9ce
edoardovincenzi <edoardo.vincenzi95@gmail.com>
2023-02-01 14:14:09 +0100

    adding react leaflet for map and introduce timeline stepper

---
+++ src/components/MapTimeBlock.tsx
@@ -0,0 +1,33 @@
+import { MapContainer, TileLayer, Marker, Tooltip } from 'react-leaflet';
+
+const MapTimeBlock = () => {
+  return (
+    <MapContainer
+      center={[45.3, 11]}
+      zoom={9}
+      scrollWheelZoom={true}
+      className="h-[40vh] w-[80vw] mx-auto mt-11 rounded-lg"
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[45.223865011261026, 10.784063829222905]}>
+        <Tooltip permanent direction="right">
+          <span className="text-sm">
+            <b>Villa corte Peron</b>
+          </span>
+        </Tooltip>
+      </Marker>
+      <Marker position={[45.353534419661756, 10.730409461364351]}>
+        <Tooltip permanent direction="right">
+          <span className="text-sm">
+            <b>Castello di Valeggio sul Mincio</b>
+          </span>
+        </Tooltip>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapTimeBlock;
