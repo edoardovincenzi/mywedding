3ee511e2bbb4b65697fd4fe49c0daaa736283817
edoardovincenzi <edoardo.vincenzi95@gmail.com>
2023-02-03 18:55:05 +0100

    set correct date

--- src/components/features/home/Home.tsx
+++ src/components/features/home/Home.tsx
@@ -21,7 +21,7 @@ const Home = () => {
       }
     >
       <Countdown
-        date={Date.now() + 500000}
+        date={1685196000000}
         renderer={(renderer) => (
           <CountdownRendered
             completed={renderer.completed}
